327e93e9c774effb0921003bd374018a23ce9ee7
heliuphy <liuhephy@gmail.com>
2020-03-15 18:51:46 +0800

    :sparkles: 测试imfill函数;

--- CannyAndFill/main.cpp
+++ CannyAndFill/main.cpp
@@ -17,9 +17,9 @@
 #include <vtkImageMathematics.h>
 #include "utils/HlwUtils.h"
 
-void findPointIDToImfill(vtkImageData *planeImageData, double *p1, double *p2) {
+void findPointIDToImfill(vtkImageData *afterCannyImageData, double *p1, double *p2) {
     int planeExtent[6];
-    planeImageData->GetExtent(planeExtent);
+    afterCannyImageData->GetExtent(planeExtent);
 
     int horizontalLineExtent[6] = {0, 0, 0, 0, 0, 0};
     horizontalLineExtent[1] = planeExtent[1];
@@ -31,7 +31,35 @@ void findPointIDToImfill(vtkImageData *planeImageData, double *p1, double *p2) {
     printArray<int>(planeExtent, 6, "planeExtent");
     printArray<int>(horizontalLineExtent, 6, "horizonExtent");
 
-    
+    int thisPoint = 0;
+    // 遇到的边界编号,从左往右应该遇到8次
+    int edgeNum = 0;
+    int edgeArray[8] = {0};
+    auto pixel = (unsigned char *) (afterCannyImageData->GetScalarPointerForExtent(horizontalLineExtent));
+    for (int i = 0; i < horizontalLineExtent[1]; i++) {
+        if (*pixel != 0) {
+            edgeArray[edgeNum] = i;
+            edgeNum++;
+        }
+        if (edgeNum == 4) {
+            cout << "Have found 4 edges, Stop !" << endl;
+            break;
+        }
+        pixel++;
+    }
+    int _y = horizontalLineExtent[2];
+    int _z = horizontalLineExtent[4];
+    int ijk[3] = {0};
+    ijk[1] = _y;
+    ijk[2] = _z;
+    vtkIdType pointIDs[4] = {0};
+    for (int i = 0; i < 4; i++) {
+        ijk[0] = edgeArray[i];
+        pointIDs[i] = afterCannyImageData->ComputePointId(ijk);
+    }
+
+    printArray<int>(edgeArray, 4, "edgeArray");
+    printArray<vtkIdType>(pointIDs, 4, "pointIDs");
 
 }
 
